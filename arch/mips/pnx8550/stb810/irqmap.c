f6ced21f3510f7b56f6d6194f64eaf26a8cda9c4
Joel Thomas <jt@emlix.com>
2011-11-25 16:27:31 +0100

    laszlos IRQ fix for the p54pci interrupts

--- arch/mips/pnx8550/stb810/irqmap.c
+++ arch/mips/pnx8550/stb810/irqmap.c
@@ -19,4 +19,5 @@ char pnx8550_irq_tab[][5] __initdata = {
 	[8]	= { -1, PNX8550_INT_PCI_INTA, 0xff, 0xff, 0xff},
 	[9]	= { -1, PNX8550_INT_PCI_INTA, 0xff, 0xff, 0xff},
 	[10]	= { -1, PNX8550_INT_PCI_INTA, 0xff, 0xff, 0xff},
+	[12]    = { -1, PNX8550_INT_PCI_INTA, 0xff, 0xff, 0xff},
 };
